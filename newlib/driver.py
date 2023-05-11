d529e19ac3fbfaf34bc49ac94e20b846c212f4a5
Jarrod Leddy <jleddy@dynamo-62.txcorp.com>
2023-05-11 14:01:35 -0600

    add new lib

---
+++ newlib/driver.py
@@ -0,0 +1,16 @@
+import stl
+from grid import Grid
+
+def runTest():
+  dim = 3
+  nx = [30,30,30]
+  sp = [0.0,0.0,0.0]
+  ep = [1.0,1.0,1.0]
+
+  # create geometry
+
+  # create grid
+
+
+if __name__ == "__main__":
+  runTest()
