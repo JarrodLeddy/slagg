5ad10601db6b3f967d06fec926b0114b67f7d911
Jarrod Leddy <jleddy@Tech-Xs-MacBook-Pro.local>
2023-05-13 15:38:13 -0600

    initial refinement to remove empty cells working very nicely

--- newlib/driver.py
+++ newlib/driver.py
@@ -8,16 +8,25 @@ def runTest():
   ep = [1.0,1.0,1.0]
 
   # create geometry
-  geom = Geometry('Moon.stl')
+  geom_name = "Torus" # Moon
+  geom = Geometry(geom_name+'.stl')
   #geom.plot()
 
   # create grid
   # grid = Grid((10,15,20),(-1.0,-1.0,-1.0),(1.0,2.0,1.0))
-  grid = Grid((17,4,30), geometry=geom)
+  if (geom_name == "Moon"):
+    grid = Grid((17,4,30), geometry=geom) # with moon
+  elif (geom_name == "Torus"):
+    grid = Grid((20,20,7), geometry=geom)
   ax = grid.plot(plot=False)
 
   decomp = Decomp(grid,12)
-  _ = decomp.plot(axes=ax, plot=True)
+  decomp.plot(axes=ax, plot=True)
+
+  # refine and plot
+  decomp.refine_empty()
+  ax = geom.plot(plot=False)
+  decomp.plot(axes=ax, plot=True)
 
 if __name__ == "__main__":
   runTest()
