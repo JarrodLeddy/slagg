ee48b87b4d3cbf89d5b6f702f45ec5abe4e3e74b
Jarrod Leddy <jleddy@Tech-Xs-MacBook-Pro.local>
2023-05-15 18:13:14 -0600

    add test and test directory

---
+++ tests/test_full_decomp.py
@@ -0,0 +1,20 @@
+import stl
+from grid import Grid, Decomp, Geometry
+
+
+def test_moon():
+    dim = 3
+
+    # create geometry
+    geom_name = "Moon"
+    geom = Geometry("../lib/stl_files/" + geom_name + ".stl")
+
+    # create grid
+    grid = Grid((20, 6, 40), geometry=geom)  # with moon
+
+    decomp = Decomp(grid, 32, geometry_biased=False)
+    decomp = Decomp(grid, 32, geometry_biased=True)
+    decomp.refine_empty(refill_empty=True)
+    decomp.refine_small()
+    decomp.diagnostics(plot=True)
+
