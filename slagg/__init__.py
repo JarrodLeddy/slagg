3694ed4d7f1a3797f958d14df7f224c35278fa3e
Jarrod Leddy <jleddy@dynamo-68.txcorp.com>
2023-05-17 10:55:54 -0600

    fix imports in init

--- slagg/__init__.py
+++ slagg/__init__.py
@@ -9,8 +9,8 @@ except PackageNotFoundError:
 
     __version__ = get_version(root="..", relative_to=__file__)
 
-from grid import Grid
-from decomp import Decomp
-from geometry import Geometry
+from slagg.grid import Grid
+from slagg.decomp import Decomp
+from slagg.geometry import Geometry
 
 __all__ = ["__version__"]
